b65c2cb4c2ab684b7b0010b075bcd4c1586aedb5
VapeStoreBro <vapestorebro2024@gmail.com>
2026-08-02 12:21:27 +0300

    Redesign buyer and admin interaction states

--- app/states.py
+++ app/states.py
@@ -3,13 +3,13 @@ from aiogram.fsm.state import State, StatesGroup
 
 class OrderFlow(StatesGroup):
     choosing_tariff = State()
-    choosing_duration = State()
-    choosing_start_mode = State()
-    entering_booking_date = State()
+    confirming_selection = State()
     waiting_post = State()
     adding_buttons = State()
+    previewing = State()
 
 
 class AdminFlow(StatesGroup):
     adding_admin = State()
+    removing_admin = State()
     setting_personal_price = State()
